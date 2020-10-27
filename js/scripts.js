870d0c6cc29b4423771fda6cd96e24aa85973f06
Amanda Otero <mandystar018@gmail.com>
2020-10-26 21:24:21 -0700

    Create an array holding variables

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
   $("#groceries").submit(function(event) {
-    event.preventDefaul();
+    event.preventDefault();
+
+    const items = [$('#item1').val(), $('#item2').val(), $('#item3').val(), $('#item4').val(), $('#item5').val()];
+    
+    items.forEach(function(element) {
+      
+    });
   });
 });
