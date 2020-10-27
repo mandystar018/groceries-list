f1f7cad930dab0a88321c9644c3bcfcec807b9d5
Amanda Otero <mandystar018@gmail.com>
2020-10-26 21:58:55 -0700

    Add list in js and  show text

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,12 @@
 $(document).ready(function(){
   $("#groceries").submit(function(event) {
     event.preventDefault();
-
     const items = [$('#item1').val(), $('#item2').val(), $('#item3').val(), $('#item4').val(), $('#item5').val()];
-    
+    items.sort();
     items.forEach(function(element) {
-      
+      $('#list').append(`<li>${element}</li>`);
+      $('#h2').text('List of items:');
+      $('#groceries').hide();
     });
   });
 });
